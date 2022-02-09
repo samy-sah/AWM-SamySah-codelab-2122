ff1e4792558b29da60aaa91eaf3787945dc624bb
samy-sah <55875247+samy-sah@users.noreply.github.com>
2022-02-09 18:01:01 +0100

    codelab 2122

---
+++ codelab-friendlychat-web-main/codelab-friendlychat-web-main/web-start/src/firebase-config.js
@@ -0,0 +1,26 @@
+/**
+ * To find your Firebase config object:
+ *
+ * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
+ * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
+ * 3. Select Config from the Firebase SDK snippet pane.
+ * 4. Copy the config object snippet, then add it here.
+ */
+ const firebaseConfig = {
+  apiKey: "AIzaSyDvDCD9bcaTNGx31Og1NcPTILpooqOj1k0",
+  authDomain: "friendlychat-6feac.firebaseapp.com",
+  databaseURL: "https://friendlychat-6feac-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "friendlychat-6feac",
+  storageBucket: "friendlychat-6feac.appspot.com",
+  messagingSenderId: "765067484464",
+  appId: "1:765067484464:web:b3d9f4d210a8c6dbb87cf5"
+};
+
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return config;
+  }
+}
